d24e33ceff4f876e7a96ae2504cacbe83b81d464
Yonathan <68878624+YonathanTE@users.noreply.github.com>
2022-05-19 10:54:23 -0400

    Starting to add some of the exercises

---
+++ Exercises/Exercise(s) for Basics/ProjectGutenburg.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author Yonathan
+ */
+public class ProjectGutenburg {
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.print("Johannes Gutenberg invented the printing press?");
+        System.out.print("He started out as a goldsmith!");
+        System.out.println("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.println("We are like a modern Gutenberg,");
+        System.out.println("printing vast amounts to the waiting console with ease.");
+    }
+}
